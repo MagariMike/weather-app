5a9c1ddd6d2bd9cd1032ff7b920fa960d5b78cd3
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-02 23:55:11 +0100

    added ForecastSummaries function, mapping and rendering Forecasts data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/app.css";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
 function App(props) {
-  const { location } = props;
+  const { location, forecasts } = props;
   return (
     <div className="App">
       <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
